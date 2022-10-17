58b8fe43b58d7e26df4ec443376c805a0cc0dd11
patrick <17029142+colemanrpatrick@users.noreply.github.com>
2022-10-17 16:09:02 -0400

    Major Updates
    major updates

--- js/main.js
+++ js/main.js
@@ -1,23 +1,3 @@
-let slideIndex = 1;
-showSlides(slideIndex);
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("slide");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].className = "slide";
-  }
-  slides[slideIndex-1].className = "slide active";
-}
 // Initialize and add the map
 function initMap() {
   // The location of Uluru
--- js/toggle.js
+++ js/toggle.js
@@ -67,57 +67,39 @@ console.log("toggle.js ready")
 /*///////////////*/
 //* ==== this toggle ===*//
 function toggleThis(element, activeClass) {
-
   var classArray = element.className.split(" ");
-
   var elementIndex = classArray.indexOf(activeClass);
-
   if (classArray.length >= 2){
-
     classArray.splice(elementIndex);
-
     element.className = classArray.join();
-
   }else{
-
     classArray.push(activeClass);
-
     element.className = classArray.join(" ");
-
   }
 }
 /*=== id toggle ===*/
 function idToggle(elementId,activeClass){
-
   var parentElement = document.getElementById(elementId);
-
   toggleThis(parentElement, activeClass);
-
 }
 //*==== parent toggle ===*//
 function parentToggle(thisElement,activeClass){
-
   var parentElement = thisElement.parentNode;
-
   toggleThis(parentElement, activeClass);
 }
 /*=== next sibling toggle ====*/
 function nextSiblingToggle(element,activeClass){
   var thisElement = element.nextElementSibling;
-
   toggleThis(thisElement, activeClass);
 }
 /*=== previous sibling toggle ====*/
 function prevSiblingToggle(prevElement,activeClass){
   var thisElement = prevElement.previousElementSibling;
-
   toggleThis(thisElement, activeClass);
 }
 /*=== tagname toggle ===*/
 function tagNameToggle(tagName,activeClass,tagNameIndex){
-
   var tagNameCollection = document.getElementsByTagName(tagName);
-
   for (var i = 0; i < tagNameCollection.length; i++) {
     var parentElement = tagNameCollection[tagNameIndex];
   }
@@ -125,45 +107,11 @@ function tagNameToggle(tagName,activeClass,tagNameIndex){
 
 }
 /*== tabs toggle ==*/
-function tabToggle(targetElement,element){
-  console.log("clicked");
-  // elIndex = number;
-  var parentClass = targetElement.parentNode.className;
-  console.log("targetElement");
+function tabToggle(number,element,activeClass){
+  elIndex = number;
   var el = document.getElementsByClassName(element);
-  // Closes all
   for (var i = 0; i < el.length; i++) {
     el[i].className = element;
   }
-  if (parentClass == element) {
-     targetElement.parentNode.className = element + ' active';
-  }
+  el[elIndex-1].className += activeClass;
 }
-/*======*/
-// var $eventListener = (function() {
-//   'use strict';
-//   if("ontouchstart" in document.documentElement === true){
-//     return "touchstart"
-//   }else{
-//     return "click";
-//   }
-// }());
-// // init
-// (function() {
-//   'use strict';
-//   document.addEventListener($eventListener,function(){
-//     outsideToggle(event,"tab");
-//   });
-// }());
-// //
-// function outsideToggle(event,elemClass){
-//   var elem = document.getElementsByClassName(elemClass)
-//   for (var i = 0; i < elem.length; i++) {
-//     if(!elem[i].contains(event.target) && !$visible(elem)){
-//       elem[i].className = elemClass;
-//     }
-//   }
-// }
-// const $visible = function(elem){
-//   return !!elem && !!(elem.offsetWidth === 0 || elem.offsetHeight === 0) ;
-// }
