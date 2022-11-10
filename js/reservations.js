0592f9601a9e54fd340a9bd0c2614c281fdb03ed
patrick <17029142+colemanrpatrick@users.noreply.github.com>
2022-11-09 19:25:09 -0500

    Participants
    participants

--- js/reservations.js
+++ js/reservations.js
@@ -21,50 +21,16 @@ console.log("reservations.js");
   var adultPriceInput = document.getElementById("package-number-input1");
   var youthPriceInput = document.getElementById("package-number-input2");
   var childPriceInput = document.getElementById("package-number-input3");
-
+  var packageObject;
 //============================================================//
-  function resetPurchaseUi(){
-
-    idToggle("purchase-window","active");
-
-    screen1.className = "";
-    screen2.className = "";
-    screen3.className = "";
-
-    datePicker.setAttribute("name","");
-
-    adultPrice.innerHTML = "";
-    youthPrice.innerHTML = "";
-    childPrice.innerHTML = "";
-
-    adultPriceInput.setAttribute("name","");
-    youthPriceInput.setAttribute("name","");
-    childPriceInput.setAttribute("name","");
-
-    for (var i = 0; i < screenHeader.length; i++) {
-      screenHeader[i].innerHTML = "";
-    };
-    for (var i = 0; i < screenFooter.length; i++) {
-      screenFooter[i].innerHTML = "";
-    };
-    var numberSpinner = document.getElementsByClassName("numberSpinnerInput");
-    for (var i = 0; i < numberSpinner.length; i++) {
-      numberSpinner[i].value = "";
-    };
-    $("#dateInput").attr('value','');
-    $("#datepicker").datepicker( "destroy" );
-
-  };
-//============================================================//
-
-//============================================================//
-  function setscreen1(el){
+  function setscreen1(arg){
+    packageObject = arg;
+    var element = arg;
+    console.log(arg);
 
     screen2.className = "";
     screen3.className = "";
     idToggle("screen-1","active");
-
-    let element = el;
 
     var templateTitle = "<header>" + element.title + "</header>";
     screenHeader[0].innerHTML += templateTitle;
@@ -72,10 +38,10 @@ console.log("reservations.js");
     //-- ||||||||||| date picker ||||||||||||| --//
     var dateToday = new Date();
     // list of specific disabled dates //
-    disabledDates = el.disabled_date;
+    disabledDates = element.disabled_date;
     for (var i = 0; i < disabledDates.length; i++) {
       disabledDates[i] = disabledDates[i].replace(/\//g, '-');
-    }
+    };
     $("#datepicker").datepicker({
       minDate: dateToday,// dates before current day disabled //
       beforeShowDay: function(date){ // disables dates based on disabledDates
@@ -111,6 +77,8 @@ console.log("reservations.js");
     document.getElementById("screen1btn").addEventListener("click",function(){
       setscreen2(element);
     });
+
+    return arg;
   };
 
 //============================================================//
@@ -143,7 +111,9 @@ var numberPlus = document.getElementsByClassName("numberPlus"),
     };
   };
 //============================================================//
-  function setscreen2(el){
+  function setscreen2(arg){
+    packageObject = arg;
+    let el = arg;
     screen1.className = "";
     screen3.className = "";
     idToggle("screen-2","active");
@@ -204,13 +174,12 @@ var numberPlus = document.getElementsByClassName("numberPlus"),
   var termsCheck;
 //============================================================//
   function setscreen3(arg){
-
+    packageObject = arg;
     screen1.className = "";
     screen2.className = "";
     idToggle("screen-3","active");
 
-    // let element = arg;
-    // function getParticipants(element)
+    showParticipants();
 
     var templateTitle = "<header>" + arg.title + "</header>";
 
@@ -220,7 +189,7 @@ var numberPlus = document.getElementsByClassName("numberPlus"),
     screenFooter[2].innerHTML += '<button type="button" class="yellow-button" id="checkout" disabled/>checkout</a>';
 
     document.getElementById("screen3back").addEventListener("click",function(){
-      setscreen2(el);
+      setscreen2(arg);
     });
 
     var $checkout = document.getElementById("checkout");
@@ -236,3 +205,172 @@ var numberPlus = document.getElementsByClassName("numberPlus"),
        alert("checkout function goes here!");
     });
   };
+//============================================================//
+//============================================================//
+//============================================================//
+var container = document.getElementById("participants"),
+		participant = document.getElementsByClassName("participant"),
+		carrier = document.getElementById("carrierInput");
+var participantsValue,
+		participantsArr,
+		num;
+
+function getParticipants() {
+
+  carrier.setAttribute("name",packageObject.participant_name);
+  carrierName = carrier.getAttribute("name");
+  var carrier_value;
+
+  if(packageObject.participants_names_ages.length > 0 && packageObject.participants_names_ages !== " "){
+    console.log("JSON");
+    carrier_value = packageObject.participants_names_ages;
+    participantsValue = carrier_value;
+
+  }else if(localStorage.getItem(packageObject.participant_name)){
+    console.log("local storage");
+
+    carrier_value = localStorage.getItem(packageObject.participant_name);
+    participantsValue = carrier_value;
+  }else{
+    participantsValue = "";
+  };
+};
+
+function showParticipants() {
+    getParticipants();
+    var count;
+    if (participantsValue !== null) {
+        container.innerHTML = "";
+        participants = participantsValue.split(",");
+        participantsArr = [];
+        participants.forEach(function (item, index) {
+            var newItem = item.split('|');
+            participantsArr.push(newItem);
+            createParticipant();
+        })
+        var nameInput = document.querySelectorAll(".input-name");
+        var ageInput = document.querySelectorAll(".input-age");
+        for (var i = 0; i < nameInput.length; i++) {
+            nameInput[i].value = "" + participantsArr[i][0] + "";
+            ageInput[i].value = "" + participantsArr[i][1] + "";
+        }
+    } else {
+        createParticipant();
+    }
+}
+var removeBtn = document.getElementsByClassName('button');
+
+function removeParticipant() {
+    this.parentElement.setAttribute("data-delete", "delete");
+};
+
+var newParticipant;
+
+function createParticipant() {
+    newParticipant = document.createElement('div');
+    newParticipant.setAttribute("class", "participant");
+    var inputText = ["name", "age"];
+    inputText.forEach(function (el) {
+        var labels = document.createElement('label');
+        labels.htmlFor = el;
+        labels.innerHTML = el;
+        newParticipant.appendChild(labels);
+    })
+    inputText.forEach(function (el,index) {
+        var inputs = document.createElement('input');
+        inputs.type = 'text';
+        inputs.setAttribute("class", "input-" + el + "");
+        inputs.setAttribute("placeholder", "" + el + "");
+        newParticipant.appendChild(inputs);
+    })
+    var removeBtn = document.createElement('button');
+    removeBtn.type = 'button';
+    removeBtn.setAttribute("class", "removeBtn");
+    removeBtn.innerHTML = '<span class="material-symbols-outlined">cancel</span>';
+    removeBtn.addEventListener('click', removeParticipant, false);
+    newParticipant.appendChild(removeBtn);
+    container.appendChild(newParticipant);
+}
+
+function storeParticipants() {
+    getParticipants();
+    var participantStr;
+    var participantArr = [];
+    var collectionArr = [];
+    for (var i = 0; i < participant.length; i++) {
+        var participantName = participant[i].querySelectorAll(".input-name")[0].value.trim();
+        var participantAge = participant[i].querySelectorAll(".input-age")[0].value;
+
+        var $noAge = " ";
+
+        var $nameValue;
+        var $ageValue;
+
+        if (!participantName || participantName === 'undefined' || participantName === " " || participantName === "" || participantName.length === 0) {
+            participant[i].setAttribute("data-delete", "delete");
+        } else {
+            $nameValue = participantName;
+        }
+
+        if (!participantAge || participantAge === 'undefined' || participantAge === " " || participantAge === "" || participantAge.length === 0) {
+            $ageValue = $noAge;
+        } else {
+            $ageValue = participantAge;
+        }
+
+        participantStr = $nameValue + "|" + $ageValue;
+
+        if (!participant[i].getAttribute("data-delete")) {
+            participantStr = participantStr.trim();
+            participantArr.push(participantStr);
+        }
+    }
+    for (var i = 0; i < participantArr.length; i++) {
+        collectionArr.push(participantArr[i]);
+    }
+
+    carrier.value = collectionArr;
+};
+//============================================================//
+  function resetPurchaseUi(){
+
+    storeParticipants();
+
+    for (var i = 0; i < participant.length; i++) {
+        if (!participant[i].getAttribute("data-delete")) {
+            localStorage.setItem(packageObject.participant_name,carrier.value);
+        }
+    }
+    console.log(localStorage.getItem(packageObject.participant_name));
+
+    idToggle("purchase-window","active");
+
+    screen1.className = "";
+    screen2.className = "";
+    screen3.className = "";
+
+    datePicker.setAttribute("name","");
+
+    adultPrice.innerHTML = "";
+    youthPrice.innerHTML = "";
+    childPrice.innerHTML = "";
+
+    adultPriceInput.setAttribute("name","");
+    youthPriceInput.setAttribute("name","");
+    childPriceInput.setAttribute("name","");
+
+    for (var i = 0; i < screenHeader.length; i++) {
+      screenHeader[i].innerHTML = "";
+    };
+    for (var i = 0; i < screenFooter.length; i++) {
+      screenFooter[i].innerHTML = "";
+    };
+    var numberSpinner = document.getElementsByClassName("numberSpinnerInput");
+    for (var i = 0; i < numberSpinner.length; i++) {
+      numberSpinner[i].value = "";
+    };
+    $("#dateInput").attr('value','');
+    $("#datepicker").datepicker( "destroy" );
+
+  };
+//============================================================//
